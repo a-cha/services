053e755173059b223fa2cea455b1acd597e2dc95
Anton Chaparin <chaparin.anton@gmail.com>
2021-01-04 22:35:56 +0300

    FTPS is working correctly now!

--- ftps.sh
+++ ftps.sh
@@ -15,13 +15,11 @@ then
   docker rmi -f ftps_image
 fi
 
-#docker build -t ftps_image _wel/srcs/ftps
 docker build -t ftps_image srcs/ftps
 
-#kubectl apply -f _wel/srcs/ftps/ftps.yaml
 kubectl apply -f srcs/ftps/ftps.yaml
 
-if [ "$1" != 'd' ] && [ "$2" != 'd' ]
+if [ "$1" != 'd' ] && [ "$2" != 'd' ] && [ "$3" != 'd' ]
 then
 	minikube dashboard
 fi
