7e4457b36bd04f694c9e317b13ef040e1b1d839b
Anton Chaparin <chaparin.anton@gmail.com>
2021-01-06 22:20:24 +0300

    wp is slow and without images

--- wp-sql-php.sh
+++ wp-sql-php.sh
@@ -8,32 +8,32 @@ if [[ $1 == 're' ]]
 then
 	kubectl delete deploy wordpress-deployment
 	kubectl delete svc wordpress-service
-	kubectl delete deploy mysql-deployment
-	kubectl delete svc mysql-service
-	kubectl delete deploy php-deployment
-  kubectl delete svc php-service
-  kubectl delete pvc --all
-  kubectl delete pv --all
+#	kubectl delete deploy mysql-deployment
+#	kubectl delete svc mysql-service
+#	kubectl delete deploy php-deployment
+#  kubectl delete svc php-service
 fi
 
 if [[ $2 == 'all' ]]
 then
+  kubectl delete pvc --all
+  kubectl delete pv --all
   docker rmi -f mysql_image
-  docker rmi -f php_image
+#  docker rmi -f php_image
   docker rmi -f wordpress_image
 fi
 
-docker build -t php_image srcs/phpmyadmin
+#docker build -t php_image srcs/phpmyadmin
 #docker build -t php_image ./_wel/srcs/php
 
 #docker build -t mysql_image ./_rus/srcs/mysql
 #docker build -t mysql_image ./_wel/srcs/mysql
- docker build -t mysql_image srcs/mysql
+# docker build -t mysql_image srcs/mysql
 
 #docker build -t wordpress_image _matr/srcs/wordpress
 #docker build -t wordpress_image _rus/srcs/wordpress
-docker build -t wordpress_image _wel/srcs/wordpress
-# docker build -t wordpress_image srcs/wordpress
+#docker build -t wordpress_image _wel/srcs/wordpress
+ docker build -t wordpress_image srcs/wordpress
 
 kubectl apply -k ./
 
